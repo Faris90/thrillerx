45d75509ed19ea699cc353857760f856ce49ce7c
( ͡° ͜ʖ ͡°) Fs unlimited <faris.soliman@yahoo.com>
2018-05-02 13:42:59 -0500

    Update multiAstroid.js

--- multiAstroid.js
+++ multiAstroid.js
@@ -830,7 +830,7 @@ function rockCollideTurret(rock, ps) {
             //console.log("remove player 2 " + ps.sessionId);
             ps.remove();
         } else {
-            ps.turret.life = ps.turret.life - damage;
+            ps.turret.life = ps.turret.life - 5;
             //io.sockets.emit("damagePlayer", {
             //    "sessionId": ps.sessionId,
             //   "life": ps.turret.life
